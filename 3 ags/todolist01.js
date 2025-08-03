cf5e066bbc7ace79ac0918503b57b1b15a63a3e6
Jaann <hamizanputra321@gmail.com>
2025-08-03 21:59:15 +0700

    update todolist 99%

---
+++ 3 ags/todolist01.js
@@ -0,0 +1,57 @@
+const prompt = require('prompt-sync')();
+let todoList = [];
+
+const tambahTugas = (todoList) => 
+{
+    let addTugas = prompt("Masukkan tugas: ");
+    todoList.push(addTugas);
+}
+const tampilkanTugas = (todoList) => 
+{
+    for(let i = 0; i < todoList.length;i++)
+    {
+        console.log(`No. ${i+1}: ${todoList[i-1]}`);
+    }
+}
+const hapusTugas = (todoList) => 
+{
+    let delTugas = prompt(`\n\nHapus tugas nomor? (1-${todoList.length}) `);
+    todoList.splice(delTugas-1, 1);
+    console.log("Tugas berhasil dihapus! ");
+}
+const updateTugas = (todoList) => 
+{
+    let NoTugas = prompt(`\n\nEdit tugas nomor? (1-${todoList.length}) `);
+    let editTugas = prompt("Masukkan tugas baru: ");
+    todoList.splice(NoTugas-1, 1, editTugas);
+    console.log("Tugas Berhasil diedit! ");
+}
+
+let pilihan = true;
+
+while (pilihan === true){
+    console.log("\n=== To Do List ===");
+    console.log("1. Tambah Item");
+    console.log("2. Lihat semua tugas");
+    console.log("3. Hapus tugas berdasarkan nomor");
+    console.log("4. Edit Tugas");
+    console.log("5. Keluar")
+    let input = Number(prompt("Masukkan pilihan (1-5): "));
+
+    if (input === 1){
+        tambahTugas(todoList);
+    } else if (input === 2){
+        tampilkanTugas(todoList);
+    } else if (input === 3){
+        hapusTugas(todoList);
+    } else if (input === 4){
+        updateTugas(todoList);
+    } else if (input === 5){
+        pilihan = false;
+    } else {
+        console.log("\nTidak valid! Cuma bisa pilihan 1-5");
+    }
+    console.log("\nThanks for using!");
+}
+
+// .splice = index 1, hapus 2 (index 1, 2), ganti 'ayam','mangga';
