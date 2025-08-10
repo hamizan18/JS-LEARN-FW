1247b0181daf04fe832c83c69733ad2538d67ea5
Jaann <hamizanputra321@gmail.com>
2025-08-10 22:59:00 +0700

    lapor error

--- 10 ags/materi/interval.js
+++ 10 ags/materi/interval.js
@@ -1,5 +1,22 @@
-let count = 0;
-let timer = setInterval(() => {
-    console.log("Woi!", count++);
-    if (count > 5) clearInterval(timer);
-}, 600);
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function lomding(){
+    let count = 0;
+    setInterval(() => {
+        process.stdout.write(`\rProgress: ${count++}/10`);
+        if (count > 10) return setInterval();
+    }, 500);
+}
+
+async function main(){
+
+    const loading = lomding();
+    console.log(loading);
+    clearInterval(loading);
+    await delay(300);
+        console.log("\nSyudone!!");
+}
+main();
+// /r = dipake disatu baris, baris sebelumnya bakal lenyap ditimpa yg baru
