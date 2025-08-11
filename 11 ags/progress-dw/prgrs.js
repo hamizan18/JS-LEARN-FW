cfae766f8c2316e3ad92df401c63ca0d212d78a1
Jaann <hamizanputra321@gmail.com>
2025-08-11 22:05:39 +0700

    1-after learn

---
+++ 11 ags/progress-dw/prgrs.js
@@ -0,0 +1,32 @@
+const fs = require('fs/promises');
+
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function loadings(){
+    const loadData = ['Loading', 'Loading.', 'Loading..', 'Loading...'];
+    let i = 0;
+    return setInterval(() => {
+        process.stdout.write(`\r${loadData[i++ % loadData.length]}`);
+    }, 300);
+}
+
+async function main(){
+
+    const loading = loadings();
+    await delay(2000);
+    clearInterval(loading);
+    console.log("\rDownload selesai!");
+
+    await delay(2000);
+
+    const data = await fs.readFile('data.json', 'utf-8');
+    const users = JSON.parse(data);
+
+    for(let user of users){
+        console.log(`Nama: ${user.nama}, Email: ${user.email}.`);
+        await delay(1000);
+    }
+}
+main();
