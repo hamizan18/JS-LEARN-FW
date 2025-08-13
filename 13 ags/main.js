6509bda82a44e95ba6d181e81b4d10400604e4ac
Jaann <hamizanputra321@gmail.com>
2025-08-13 22:36:59 +0700

    99% progress

--- 13 ags/main.js
+++ 13 ags/main.js
@@ -3,6 +3,18 @@ const fs = require('fs/promises');
 const app = express();
 const PORT = 3000;
 
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+function loading() {
+    const frames = ['Mencari', 'Mencari.', 'Mencari..', 'Mencari...'];
+    let i = 0;
+    return setInterval(() => {
+        process.stdout.write(`\r${frames[i++ % frames.length]}`);
+    }, 300);
+};
+
 app.get('/', (req, res) => {
     res.send('<h1>Haii sekaiii</h1><p>Namakuu mijann salken yhh..</p>');
 });
@@ -17,6 +29,14 @@ app.get('/data', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server sedang dijalankan di http://localhost:${PORT}`);
+app.get('/about', (req, res) => {
+    res.send('<h1>Nama saya Hamizan</h1><p>Iya kita semua juga tau yahahha</p>');
+});
+
+app.listen(PORT, async () => {
+    const loadings = loading();
+    await delay(1500);
+    clearInterval(loadings);
+    process.stdout.write('\r');
+    console.log(`\nServer sedang dijalankan di http://localhost:${PORT}`);
 });
